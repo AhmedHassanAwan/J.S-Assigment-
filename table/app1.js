57f948532831d624a66827990ee05bc17d5dd965
AhmedHasssanAwan <awanhasan2007@gmail.com>
2024-09-15 00:59:35 -0700

    Table

---
+++ table/app1.js
@@ -0,0 +1,9 @@
+
+var userinput = +prompt("enter your value");
+ 
+ var head = document.querySelector(.head);
+ 
+
+for (var i = 1; i<=10; i++) {
+    console.log(`${userinput} x ${i} = ${userinput*i}`)
+}
